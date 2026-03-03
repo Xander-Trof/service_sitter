25776125c3f05532ccbccf9edbb4d8d1c0d7cd11
atrofimov <lextrofimov98@gmail.com>
2026-03-03 19:27:02 +0300

    Перенести docker команды в отдельные файлы

--- dockercomands/docker_ps.go
+++ dockercomands/docker_ps.go
@@ -6,25 +6,18 @@ import (
 	"github.com/moby/moby/client"
 )
 
-func DockerPS() []string {
+
+func DockerPS() client.ContainerListResult {
     cli, err := client.New(client.FromEnv)
     if err != nil {
         panic(err)
     }
     defer cli.Close()
 
-    
     containersListResult, err := cli.ContainerList(context.Background(), client.ContainerListOptions{})
     if err != nil {
         panic(err)
     }
 
-    containers := containersListResult.Items
-    
-    containerNames := make([]string, len(containers))
-    for i, container := range containers {
-        containerNames[i] = container.Names[0]
-    }
-
-    return containerNames
+    return containersListResult
 }
